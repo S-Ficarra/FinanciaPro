c50e9ab89446ddd455af057a9b50e096768c76e0
Sylvain Ficarra <contact@ficarra.fr>
2025-06-03 16:17:30 +0200

    db: added h2 db to project

--- src/main/java/com/financiaPro/app/models/User.java
+++ src/main/java/com/financiaPro/app/models/User.java
@@ -14,7 +14,10 @@ public class User {
     private int id;
     private String firstName;
     private String name;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
     private String apiKey;
 
 }
