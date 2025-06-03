d3e54b5b9ac7c8ee12639b900d57e8fb9190b599
Sylvain Ficarra <contact@ficarra.fr>
2025-06-03 16:17:30 +0200

    db: added h2 db to project

---
+++ src/main/java/com/financiaPro/app/service/UserService.java
@@ -0,0 +1,21 @@
+package com.financiaPro.app.service;
+
+import com.financiaPro.app.models.User;
+import com.financiaPro.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
