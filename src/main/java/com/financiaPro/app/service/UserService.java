abce3c31903e4a3814105215d065d7325d886446
Sylvain Ficarra <Admin@MacBook-Air.local>
2025-06-04 16:54:23 +0200

    feat: random api key generation added

--- src/main/java/com/financiaPro/app/service/UserService.java
+++ src/main/java/com/financiaPro/app/service/UserService.java
@@ -1,11 +1,15 @@
 package com.financiaPro.app.service;
 
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.financiaPro.app.models.User;
 import com.financiaPro.app.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import java.util.Optional;
+
 
 @Service
 public class UserService {
@@ -13,7 +17,16 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    public static String generateApiKey() {
+        SecureRandom random = new SecureRandom();
+        byte[] bytes = new byte[32]; // 256 bits
+        random.nextBytes(bytes);
+        return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
+    }
+
     public User createUser(User user) {
+
+        user.setApiKey(generateApiKey());
 
         Optional<User> alreadyExisting = userRepository.findByEmail(user.getEmail());
 
