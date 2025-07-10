01a913f53ab004833a243a032511326141975ecf
Sylvain Ficarra <contact@ficarra.fr>
2025-07-10 17:08:17 +0200

    fix: authorisation header added in swagger

--- src/main/java/com/financiaPro/app/config/SwaggerConfig.java
+++ src/main/java/com/financiaPro/app/config/SwaggerConfig.java
@@ -2,18 +2,31 @@ package com.financiaPro.app.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "ApiKeyAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .info(new Info()
-                .title("FinanciaPro API")
-                .version("1.0")
-                .description("Documentation de l'API utilisateur"));
+                .info(new Info()
+                        .title("FinanciaPro API")
+                        .version("1.0")
+                        .description("Documentation de l'API utilisateur"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name("X-API-KEY") // Le nom du header
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name(SECURITY_SCHEME_NAME)));
     }
 }
